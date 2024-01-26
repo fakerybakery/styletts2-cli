bb630e6676f6719cd53820adada7e52f0d0a2cab
mrfakename <me@mrfake.name>
2024-01-26 14:17:54 -0800

    Fixes

--- cli.py
+++ cli.py
@@ -7,9 +7,9 @@ import click
 @click.option('--pth-path', '-p', type=click.Path(exists=True), required=True)
 @click.option('--file', '-f', default=False, show_default=True, help='Read from input text from file')
 def main(text, output, audio_sample, pth_path, file):
-    if f:
-        with open(text, 'r') as f:
-            text = f.read()
+    if file:
+        with open(text, 'r') as file:
+            text = file.read()
     click.echo("Importing modules...")
     import msinference
     from pydub import AudioSegment
